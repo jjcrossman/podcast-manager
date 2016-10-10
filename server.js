0b1823a4d8e9a15ebef4194486c6779010580a1f
JJ Crossman <jj.crossman@gmail.com>
2016-10-10 15:25:19 -0500

    stuff

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require( "express" );
+const app = express();
+const { json } = require( "body-parser" );
+const mongoose = require( "mongoose" );
+const cors = require( "cors" );
+const port = 4000;
+// const mongoUri = "mongodb://localhost:27017/podcast-manager";
+
+// USE
+app.use( cors() );
+app.use( json() );
+app.use( express.static( `${ __dirname }/public` ) );
+
+// Mongoose connection to MongoDB
+
+// Master Routes
+// require( "./masterRoutes.js" )( app );
+
+// LISTEN
+
+app.listen( port, () => console.log( `Podcast Manager is listening on port ${ port }` ) );
