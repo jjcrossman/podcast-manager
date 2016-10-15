0949de0f044758ab90b1e54d52ee0f91322b2741
JJ Crossman <jj.crossman@gmail.com>
2016-10-15 00:37:56 -0500

    finished episode expansion skeleton

--- public/src/views/controllers/mineCtrl.js
+++ public/src/views/controllers/mineCtrl.js
@@ -1,8 +1,18 @@
 function mineCtrl( $scope ) {
 
   function init() {
-    
+    $scope.activeLi = "";
   }
+
+  $scope.toggleActiveLi = () => {
+    console.log( "toggleActiveLi fired" );
+    if ( $scope.activeLi ) {
+      $scope.activeLi = "";
+      console.log( "pass" );
+    } else {
+      $scope.activeLi = "pm-li-active";
+    }
+  };
 
   init();
 
